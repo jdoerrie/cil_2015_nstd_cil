fce6207cfaa7a10e37dac9012bf25ce90fce0f4f
dojan <dojan@ethz.ch>
2015-06-08 14:22:57 +0200

    Modified regSVD

--- project/src/PredictMissingValues.m
+++ project/src/PredictMissingValues.m
@@ -1,4 +1,4 @@
-function X_pred = PredictMissingValues(X, nil, k)
+function X_pred = PredictMissingValues(X, nil)
 % Predict missing entries in matrix X based on known entries. Missing
 % values in X are denoted by the special constant value nil.
 
@@ -16,73 +16,5 @@ else
   X(nils) = NaN;
 end
 
-if nargin < 3
-  % best k for the new Data.mat
-  k = 6;
-end
-
-X_pred = AsymmetricSVD(X, k);
-% lambda = 0.1;
-% % for i=1:20
-%   [mu, bu, bi] = LearnBiases(X, lambda);
-%   BaseLine = mu + bsxfun(@plus, bu, bi');
-%   % fprintf('lambda: %f\n', lambda);
-%   for k=6:8
-%     [P,Q,mu,bu,bi] = LearnVectors(X, k);
-%     X_pred = P*Q' + mu + bsxfun(@plus,bu, bi');
-%     X_pred = X - BaseLine;
-%     X_pred(nils) = 0;
-%     % Item Based Clusters
-%     [IDX, C] = kmeans(X_pred', k);
-%     X_pred(nils) = NaN;
-%     for i=1:k
-%       idx = IDX == i;
-
-%     % X_pred = TruncatedSVD(X_pred, k);
-%     % X_pred = X_pred + BaseLine;
-%     X_pred = min(max(X_pred, 1), 5);
-%     fprintf('k = %d; err = %f\n', k, RMSE(X_pred));
-%   end
-  % lambda = lambda * 2;
-% end
-% [P, Q, mu, bu, bi] = LearnVectors(X, k);
-% X_pred = P*Q' + bsxfun(@plus, bu, bi');
-% [mu, b_u, b_i] = GetBiases(X);
-% means = bsxfun(@plus, b_u, b_i) + mu;
-% X_pred(nils) = means(nils);
-% [P, Q, mu, bu, bi] = LearnVectors(X, nil, k, 0.01, 0);
-% X_pred = mu + bsxfun(@plus, bu, bi') + P*Q';
-% X_pred(~nils) = X(~nils);
-
-% try to approximate missing b_ui by mu + b_u + b_i
-% idea taken from "The BellKor Solution to the Netflix Grand Prize"
-% if isempty(mu)
-%   [mu, b_u, b_i] = GetBiases(X, NaN);
-% end
-
-% means = bsxfun(@plus, b_u, b_i) + mu;
-% % means might contain NaNs if a full row or column is missing
-% means(isnan(means)) = 0;
-
-% subtract means from X
-% X_norm = X - means;
-% X_norm(nils) = 0;
-
-% truncated SVD
-% [U, D, V] = svd(X_norm, 0);
-% X_norm = U(:,1:k) * D(1:k,1:k) * V(:,1:k)';
-% X_norm = X_norm + means;
-
-% X_pred = X;
-% X_pred(nils) = X_norm(nils);
-% fill in nils with means
-% X_pred = X;
-% X_pred(nils) = means(nils);
-
-% % truncated SVD
-% [U, D, V] = svd(X_pred, 0);
-% res = U(:,1:k) * D(1:k,1:k) * V(:,1:k)';
-
-% % set nils to predicted values
-% X_pred(nils) = res(nils);
+X_pred = regSVD(X);
 end
--- project/src/regSVD.m
+++ project/src/regSVD.m
@@ -3,90 +3,59 @@ function X_pred = regSVD(X, K, gamma, lambda_1, lambda_2)
   % Neighborhood: a Multifaceted Collaborative Filtering Model" Section 2.3
 
   % Hyperparameters and default values
-  if (nargin < 2)         K = 6; end % number of factors
-  if (nargin < 3)  gamma = 0.01; end % learning rate
-  if (nargin < 4) lambda_1 = 0.02; end % regularizer term
+  if (nargin < 2)           K = 6; end % number of factors
+  if (nargin < 3)   gamma = 0.010; end % learning rate
+  if (nargin < 4) lambda_1 = 0.10; end % regularizer term
   if (nargin < 5) lambda_2 = 0.10; end % regularizer term for biases
 
+  % number of iterations over all known ratings per factor
+  nEpochs = 100;
   % Dimensions of the input
   [M, N] = size(X);
-
-  mu = zeros(1, K); % global bias per factor
-  bu = zeros(M, K); % user biases per factor
-  bi = zeros(N, K); % item biases per factor
-  P  = zeros(M, K); % user latent factors
-  Q  = zeros(N, K); % item latent factors
 
   % U and I contain indices into X where ratings are available
   [U, I] = find(~isnan(X));
 
-  % initialize return value
+  % initialize predictions
   X_pred = zeros(M,N);
-
-  % stopping criterion
-  delta = 1e-4;
-
   for k=1:K
-    % init error term
-    old_err = Inf;
+    fprintf('Training Factor %d\n', k);
+    mu = nanmean(X(:) - X_pred(:));
+    bu = zeros(M,1);
+    bi = zeros(N,1);
 
-    P(:,k) = ones(M, 1) * 0.1;
-    Q(:,k) = ones(N, 1) * 0.1;
-    fprintf('Training Feature %d\n', k);
+    % 0.1 to initialize is inspired by Simon Funk
+    p  = ones(M,1) * 0.1;
+    q  = ones(N,1) * 0.1;
 
-    mu(k) = nanmean(X(:));
-    for epoch=1:10
+    for epoch=1:nEpochs
       % Iterate over all known ratings
       for idx=1 : length(U)
-        if mod(idx, 1e5) == 0
-          fprintf('epoch: %d, iter: %d\n', epoch, idx);
-        end
-
-        u = U(idx);             % current user
-        i = I(idx);             % current item
-        pu = P(u,k);            % current latent factors for user u
-        qi = Q(i,k);            % current latent factors for item i
+        u = U(idx); % current user
+        i = I(idx); % current item
+        pu = p(u);  % current latent factors for user u
+        qi = q(i);  % current latent factors for item i
 
         % approximation and error term
-        r_hat = mu(k) + bu(u,k) + bi(i,k) + P(u,k) * Q(i,k);
+        r_hat = X_pred(u,i) + mu + bu(u) + bi(i) + pu * qi;
         e_ui = X(u,i) - r_hat;
 
         % gradient updates
-        bu(u,k) = bu(u,k) + gamma * ( e_ui      - lambda_2 * bu(u,k) );
-        bi(i,k) = bi(i,k) + gamma * ( e_ui      - lambda_2 * bi(i,k) );
-        P(u,k)  = P(u,k)  + gamma * ( e_ui * qi - lambda_1 * P(u,k)  );
-        Q(i,k)  = Q(i,k)  + gamma * ( e_ui * pu - lambda_1 * Q(i,k)  );
+        bu(u) = bu(u) + gamma*( e_ui    - lambda_2*bu(u) );
+        bi(i) = bi(i) + gamma*( e_ui    - lambda_2*bi(i) );
+        p(u)  = pu    + gamma*( e_ui*qi - lambda_1*pu    );
+        q(i)  = qi    + gamma*( e_ui*pu - lambda_1*qi    );
       end
 
       % compute predictions
-      X_curr = getRatings(mu,bu,bi,P,Q);
-      new_err = RMSE(X_curr);
-      fprintf('Curr RMSE: %f\n', new_err);
-      if old_err < new_err
-        %break
-      end
-
-      X_pred = X_curr;
-      if abs(old_err - new_err) < delta
-        %break;
-      end
-
-      old_err = new_err;
+      X_curr = X_pred + mu + bsxfun(@plus, bu, bi') + p*q';
+      X_curr = min(max(X_curr, 1), 5);
+      fprintf('Epoch: %03d, Curr RMSE: %f\n', epoch, RMSE(X_curr));
     end
 
-    X = X - (mu(k) + bsxfun(@plus, bu(:,k), bi(:,k)') + P(:,k) * Q(:,k)');
+    X_pred = X_pred + mu + bsxfun(@plus, bu, bi') + p*q';
+    X_pred = min(max(X_pred, 1), 5);
     fprintf('rSVD, K = %d, gamma = %f, lambda = %f, rmse = %f\n', ...
       k, gamma, lambda_1, RMSE(X_pred));
-  end
-end
-
-function X_pred = getRatings(mu, bu, bi, P, Q)
-  M = size(P, 1);
-  N = size(Q, 1);
-  K = size(P, 2);
-  X_pred = zeros(M,N);
-  for k=1:K
-    X_pred = X_pred + mu(k) + bsxfun(@plus, bu(:,k), bi(:,k)') + P(:,k) * Q(:,k)';
-    X_pred = min(max(X_pred, 1), 5);
   end
 end
