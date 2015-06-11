cff5c6a1d78a0299119f08e81b2d6e28f6aca044
Jan Wilken Doerrie <dojan@ethz.ch>
2015-06-11 13:20:25 +0200

    Added Global Flag

--- project/src/SVDpp.m
+++ project/src/SVDpp.m
@@ -1,16 +1,21 @@
-function X_pred = SVDpp2(X, K, gamma, lambda)
+function X_pred = SVDpp(X, K, gamma, lambda, shrink)
   % Implementation of SVD++ as seen in "Factorization Meets the
   % Neighborhood: a Multifaceted Collaborative Filtering Model"
+
+  % Boolean to switch between local and judge mode
+  is_local = false;
 
   % Hyperparameters and default values
   if (nargin < 2)         K = 64; end % number of factors
   if (nargin < 3)  gamma = 0.010; end % learning rate
   if (nargin < 4) lambda = 0.100; end % regularizer term
+  if (nargin < 5) shrink = 1.000; end % shrinkage term
 
+  orig_gamma = gamma;
   [~,~,~,B] = LearnBiases(X);
   X_n = (X - B);
   % number of iterations over all known ratings per factor
-  nEpochs = 50;
+  nEpochs = 5;
   % Dimensions of the input
   [M, N] = size(X);
 
@@ -32,11 +37,14 @@ function X_pred = SVDpp2(X, K, gamma, lambda)
   % [mu,bu,bi] = ComputeBiases(X - X_pred);
 
   % 0.1 to initialize is inspired by Simon Funk
-  p  = randn(K,M) * 0.001;
-  q  = randn(K,N) * 0.001;
-  y  = randn(K,N) * 0.001;
+  p  = randn(K,M) * 0.01;
+  q  = randn(K,N) * 0.01;
+  y  = randn(K,N) * 0.01;
 
-  X_prev = zeros(M,N);
+  if is_local
+    X_prev = zeros(M,N);
+  end
+
   for epoch=1:nEpochs
     % Iterate over all known ratings
     for u=1:M
@@ -46,46 +54,48 @@ function X_pred = SVDpp2(X, K, gamma, lambda)
 
       errors = X_n(u,Ru) - p2' * q(:,Ru);
       err_sum = q(:,Ru) * errors';
-      q(:,Ru) = q(:,Ru) + gamma * (p2 * errors - lambda * q(:,Ru));
-      p(:,u)  = pu    + gamma*( err_sum          - lambda*pu );
+      q(:,Ru) = q(:,Ru) + gamma*(p2 * errors - lambda * q(:,Ru));
+      p(:,u)  = pu      + gamma*( err_sum        - lambda*pu );
       y(:,Ru) = y(:,Ru) + gamma*( bsxfun(@minus, err_sum*isqrt(u), lambda*y(:,Ru)));
     end
 
-    gamma = gamma * 0.98;
-%    gamma = gamma / sqrt(sqrt(3));
-    % compute predictions
-    p2 = zeros(K,M);
-    for u=1:M
-      Ru = R{u};
-      p2(:,u) = isqrt(u) * sum(y(:,Ru),2);
-    end
+    gamma = gamma * shrink;
 
-    X_curr = B + (p + p2)' * q;
-    X_curr = min(max(X_curr, 1), 5);
-    % fprintf('Epoch: %02d\n', epoch);
-    fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', epoch, RMSE(X_curr), gamma);
-    if (RMSE(X_curr) > RMSE(X_prev) - 1e-6)
-%      break;
-    end
+    if is_local
+      % compute predictions
+      p2 = zeros(K,M);
+      for u=1:M
+        Ru = R{u};
+        p2(:,u) = isqrt(u) * sum(y(:,Ru),2);
+      end
 
-    X_prev = X_curr;
+      X_curr = B + (p + p2)' * q;
+      X_curr = min(max(X_curr, 1), 5);
+      % fprintf('Epoch: %02d\n', epoch);
+      if (RMSE(X_curr) > RMSE(X_prev) - 1e-6)
+        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', epoch, RMSE(X_prev), gamma);
+        break;
+      end
+
+      if (mod(epoch, 5) == 0)
+        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', epoch, RMSE(X_curr), gamma);
+      end
+
+      X_prev = X_curr;
+    end
   end
 
-  % p2 = zeros(M,1);
-  % for u=1:M
-  %   Ru = R{u};
-  %   p2(u) = isqrt(u) * sum(y(Ru));
-  % end
+  if is_local
+    X_pred = X_prev;
+    X_pred = min(max(X_pred, 1), 5);
+    fprintf('SVD++, K = %d, gamma = %f, lambda = %f, shrinkage = %f, rmse = %f\n', ...
+      K, orig_gamma, lambda, shrink, RMSE(X_pred));
+  else
+    for u=1:M
+      Ru = R{u};
+      p(:,u) = p(:,u) + isqrt(u) * sum(y(:,Ru),2);
+    end
 
-  % X_pred = X_pred + (p + p2)*q';
-  % p2 = zeros(K,M);
-  % for u=1:M
-  %   Ru = R{u};
-  %   p2(:,u) = isqrt(u) * sum(y(:,Ru),2);
-  % end
-  % X_pred = B + (p + p2)' * q;
-  X_pred = X_prev;
-  X_pred = min(max(X_pred, 1), 5);
-  fprintf('SVD++2, K = %d, gamma = %f, lambda = %f, rmse = %f\n', ...
-    K, gamma, lambda, RMSE(X_pred));
+    X_pred = min(max(B + p' * q, 1), 5);
+  end
 end
