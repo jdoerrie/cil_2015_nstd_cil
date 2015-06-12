e4a5a4a23d32d230d32f3c93b74a85220e090fad
dojan <dojan@ethz.ch>
2015-06-12 16:24:51 +0200

    Documented the shit out of SVD++

--- project/src/SVDpp.m
+++ project/src/SVDpp.m
@@ -1,101 +1,198 @@
 function X_pred = SVDpp(X, K, gamma, lambda, shrink)
-  % Implementation of SVD++ as seen in "Factorization Meets the
-  % Neighborhood: a Multifaceted Collaborative Filtering Model"
+  % Matlab optimized implementation of SVD++ as seen in "Factorization
+  % Meets the Neighborhood: a Multifaceted Collaborative Filtering Model".
+  % An approximation of the ratings is achieved through the following
+  % formula:
+  %
+  % $$ \^{r}_{ui} = \mu + b_i + b_u + q_i^T \left( p_u +
+  % |R(u)|^{-\frac{1}{2}} \sum_{j \in R(u)} y_j \right ) $$
+  %
+  % where \mu is the global average rating, b_i an item specific bias, b_u
+  % a user specific bias, q_i an item specific latent factor vector, p_u a
+  % user specific latent factor vector and R(u) the index set of ratings
+  % issued by a given user. y_j is an item specific factor vector, so that
+  % users are also specified by the set of items they rate.
 
-  % Boolean to switch between local and judge mode
-  is_local = false;
+  % Boolean to switch between local and judge mode. Local mode will print
+  % debug statements and compute the current RMSE score after every
+  % iteration through the data points.  Judge mode disables all of this,
+  % resulting in a faster algorithm but no progress reporting during the
+  % run.
+  is_local = true;
 
   % Hyperparameters and default values
-  if (nargin < 2)         K = 64; end % number of factors
-  if (nargin < 3)  gamma = 0.010; end % learning rate
-  if (nargin < 4) lambda = 0.100; end % regularizer term
-  if (nargin < 5) shrink = 1.000; end % shrinkage term
+  if nargin < 2; K      =    64; end % number of latent factors
+  if nargin < 3; gamma  = 0.010; end % learning rate
+  if nargin < 4; lambda = 0.100; end % regularizer term
+  if nargin < 5; shrink = 1.000; end % gamma shrinkage factor
 
+  % Stores the original value of gamma to be able to log it later.  The
+  % continued multiplication with the shrinkage term makes it impossible to
+  % retrieve the original value otherwise.
   orig_gamma = gamma;
-  [~,~,~,B] = LearnBiases(X);
-  X_n = (X - B);
-  % number of iterations over all known ratings per factor
-  nEpochs = 5;
-  % Dimensions of the input
+
+  % Boolean flag indicating whether biases should be learned during the
+  % gradient descent or whether just precomputed values should be used.
+  % In either case biases are computed, since they will serve as initial
+  % values even when learned later on.
+  learn_biases = false;
+
+  [mu, bu, bi, B] = ComputeBiases(X);
+
+  % X "normalized".  Stores the residuals after subtracting the biases from
+  % X.  The remaining code tries to approximate these residuals as good as
+  % possible.
+  if ~learn_biases
+    X_n = X - B;
+  end
+
+  % Define the number of epochs.  An epoch in this context is a complete
+  % iteration over all present ratings in X.
+  nEpochs = 25;
+
+  % Determine the size of the input.  M is the number of users, N the
+  % number of items.
   [M, N] = size(X);
 
-  % nRatings contains for every user the number of issued ratings
+  % Determine for each user the number of ratings he issued.
+  % $$ nRatings \in R^M $$
   nRatings = sum(~isnan(X), 2);
 
-  % isqrt is the inverse sqrt of numRatings used for normalizing the sums
-  % during the update steps
+  % Precompute for each user the inverse square root of the number of
+  % ratings issued. This will be used to normalize sums in the gradient
+  % update steps.
+  % $$ isqrt \in R^M $$
   isqrt = 1.0 ./ sqrt(max(nRatings, 1));
 
-  % R contain for every user the indices into X where ratings are available.
-  % Since the numbers of these indices are different for every user, we store
-  % the result in a cell array.
+  % For each user determine the indices into X where ratings are available.
+  % This allows for fast batch processing of all issued ratings.  The
+  % indices need to be stored in a cell array because the number for each
+  % user is different.
+  % $$ R{i} \in R^{R_i} $$
   R = cell(M, 1);
   for i=1:M
     R{i} = find(~isnan(X(i,:)));
   end
 
-  % [mu,bu,bi] = ComputeBiases(X - X_pred);
+  % P(:,u), Q(:,i) and Y(:,i) are user latent, item latent and item factor
+  % vectors. All of them are learned through a gradient descent algorithm
+  % and initialized with a gaussian distribution with mean 0 and standard
+  % deviation 0.01.
+  P = randn(K,M) * 0.01;
+  Q = randn(K,N) * 0.01;
+  Y = randn(K,N) * 0.01;
 
-  % 0.1 to initialize is inspired by Simon Funk
-  p  = randn(K,M) * 0.01;
-  q  = randn(K,N) * 0.01;
-  y  = randn(K,N) * 0.01;
-
+  % If local mode is enabled we keep track of the previous iteration's
+  % ratings approximation to be able to measure the difference in RMSE
+  % between two consecutive iterations.
   if is_local
     X_prev = zeros(M,N);
   end
 
-  for epoch=1:nEpochs
+  % Actual implementation of Gradient Descent.  We aim to minize the
+  % following objective function:
+  %
+  % $$ \sum_{(u,i) \in \mathcal{K}} r_{ui} - \^{r}_{ui} + lambda *
+  %       ( ||q_i||^2 + ||p_u||^2 + |R(u)|^{-1/2}\sum_{j \in R(u)} )
+  % ||y_j||^2 $$ leading to the following partial derivatives and update
+  % rules for a given datapoint (u,i), where e_ui = r_ui - ^r_ui.
+  %
+  % q_i += gamma*( e_ui*( p_u + |R(u)|^{-1/2} \sum_{j \in R(u) y_j} )
+  %                                       - lambda*q_i )
+  % p_u += gamma*( e_ui*q_i               - lambda*p_u )
+  % \forall j \in R(u):
+  % y_j += gamma*( e_ui*|R(u)|^{-1/2}*q_i - lambda*y_j )
+  %
+  % For efficiency reasons we will not update after each processed data
+  % point, but do a batch process after processing all rated items for a
+  % given user.
+
+  for iEpoch=1:nEpochs
     % Iterate over all known ratings
     for u=1:M
+      % indices of the ratings of user u
       Ru = R{u};
-      pu = p(:,u);  % current latent factors for user u
-      p2 = pu + isqrt(u) * sum(y(:,Ru),2);
 
-      errors = X_n(u,Ru) - p2' * q(:,Ru);
-      err_sum = q(:,Ru) * errors';
-      q(:,Ru) = q(:,Ru) + gamma*(p2 * errors - lambda * q(:,Ru));
-      p(:,u)  = pu      + gamma*( err_sum        - lambda*pu );
-      y(:,Ru) = y(:,Ru) + gamma*( bsxfun(@minus, err_sum*isqrt(u), lambda*y(:,Ru)));
+      % modified p(u) vector that takes the normalized sum of the y weights
+      % into account
+      pu = P(:,u) + isqrt(u) * sum(Y(:,Ru), 2);
+
+      % user u's known ratings
+      r_u = X(u,Ru);
+
+      % current aproximation including baseline estimators and modified
+      % latent vectors
+      rhat_u = B(u,Ru) + pu' * Q(:,Ru);
+
+      % current error terms, size(e_u) = [R, 1]
+      e_u = (r_u - rhat_u)';
+
+      % update current user latent vector, size(P(:,u)) = [K, 1]
+      P(:,u)  = P(:,u)  + gamma*( Q(:,Ru)*e_u - lambda*P(:,u) );
+
+      % update current items factor vectors, size(Y(:,Ru)) = [K, R].
+      % bsxfun is necessary, because size(Q(:,Ru) * e_u) = [K, 1] and we
+      % want to subtract the regularizer term for every item in R(u).
+      Y(:,Ru) = Y(:,Ru) + gamma* ...
+        ( bsxfun(@minus, isqrt(u)*Q(:,Ru)*e_u, lambda*Y(:,Ru)) );
+
+      % update latent item factor vectors, size(Q(:,Ru)) = [K, R]
+      Q(:,Ru) = Q(:,Ru) + gamma*(pu*e_u' - lambda * Q(:,Ru));
     end
 
+    % shrink gamma according to the specified factor
     gamma = gamma * shrink;
 
+    % if running in local mode, compute the prediction for the current
+    % iteration. V contains the modifed user latent factors and gets
+    % multiplied with Q. The current prediction is then B + Q'*P and gets
+    % clamped to be within [1,5]. Then an RMSE score is computed and
+    % printed to stdout every couple of iterations. If the score plus an
+    % epsilon was worse than the previous iteration we stop the gradient
+    % descent.
     if is_local
-      % compute predictions
-      p2 = zeros(K,M);
+      V = zeros(K,M);
       for u=1:M
         Ru = R{u};
-        p2(:,u) = isqrt(u) * sum(y(:,Ru),2);
+        V(:,u) = P(:,u) + isqrt(u) * sum(Y(:,Ru),2);
       end
 
-      X_curr = B + (p + p2)' * q;
+      X_curr = B + V'*Q;
       X_curr = min(max(X_curr, 1), 5);
-      % fprintf('Epoch: %02d\n', epoch);
-      if (RMSE(X_curr) > RMSE(X_prev) - 1e-6)
-        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', epoch, RMSE(X_prev), gamma);
-        break;
+
+      if (RMSE(X_curr) + 1e-6 > RMSE(X_prev))
+        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', iEpoch, ...
+                RMSE(X_prev), gamma);
+       break;
       end
 
-      if (mod(epoch, 5) == 0)
-        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', epoch, RMSE(X_curr), gamma);
+      if (mod(iEpoch, 5) == 0)
+        fprintf('Epoch: %03d, Curr RMSE: %f, Gamma: %f\n', iEpoch, ...
+                RMSE(X_curr), gamma);
       end
 
       X_prev = X_curr;
     end
   end
 
+  % This part assigns the computed predictions to the output variable.  If
+  % running in local mode this is equivalent to the last X_prev, so it
+  % simply assigns the value and again prints the parameters plus RMSE
+  % score.  If running in judge mode only the predictions are computed and
+  % returned.
   if is_local
     X_pred = X_prev;
-    X_pred = min(max(X_pred, 1), 5);
-    fprintf('SVD++, K = %d, gamma = %f, lambda = %f, shrinkage = %f, rmse = %f\n', ...
-      K, orig_gamma, lambda, shrink, RMSE(X_pred));
+    fprintf(
+      'SVD++, K = %d, gam = %f, lam = %f, shrink = %f, RMSE = %f\n', ...
+       K, orig_gamma, lambda, shrink, RMSE(X_pred)
+    );
   else
     for u=1:M
       Ru = R{u};
-      p(:,u) = p(:,u) + isqrt(u) * sum(y(:,Ru),2);
+      P(:,u) = P(:,u) + isqrt(u)*sum(Y(:,Ru), 2);
     end
 
-    X_pred = min(max(B + p' * q, 1), 5);
+    X_pred = B + P'*Q;
+    X_pred = min(max(X_pred, 1), 5);
   end
 end
