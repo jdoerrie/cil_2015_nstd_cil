deaa1e21441bb1d1e1484e14396916ee59b21e8b
Jan Wilken Doerrie <dojan@ethz.ch>
2015-06-13 16:36:49 +0200

    Grid Search

--- project/src/PredictMissingValues.m
+++ project/src/PredictMissingValues.m
@@ -1,21 +1,28 @@
 function X_pred = PredictMissingValues(X, nil)
-% Predict missing entries in matrix X based on known entries. Missing
-% values in X are denoted by the special constant value nil.
+  % Predict missing entries in matrix X based on known entries. Missing
+  % values in X are denoted by the special constant value nil.
 
-% your collaborative filtering code here!
+  % your collaborative filtering code here!
 
-if nargin < 2
-  nil = 0;
-end
+  if nargin < 2
+    nil = 0;
+  end
 
-% replace nils with NaNs
-if isnan(nil)
-  nils = isnan(X);
-else
-  nils = (X == nil);
-  X(nils) = NaN;
-end
+  % replace nils with NaNs
+  if isnan(nil)
+    nils = isnan(X);
+  else
+    nils = (X == nil);
+    X(nils) = NaN;
+  end
 
-X_pred = CorNgbr(X);
-RMSE(X_pred)
+  range = [1e-3, 2e-3, 5e-3, 1e-2, 2e-2, 5e-2, 1e-1, 2e-1, 5e-1];
+  shrink = [0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.90, 0.925, 0.95, 0.975, 1.00];
+  for gam=range
+    for lam=range
+      for sh=shrink
+        X_pred = SVDpp(X, 64, gam, lam, sh);
+      end
+    end
+  end
 end
