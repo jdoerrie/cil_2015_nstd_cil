3b02654b15da6445565791d4ed2f2bde516e7b75
Dina Zverinski <zdina@student.ethz.ch>
2015-06-16 01:48:45 +0200

    my pca

--- dina/PredictMissingValues.m
+++ dina/PredictMissingValues.m
@@ -1,20 +1,21 @@
-function X_pred = PredictMissingValues(X, nil, p)
+function X_pred = PredictMissingValues(X, nil)
 warning('off','all');
-X_pred = X;
-[u, it] = size(X_pred);
-nils = X_pred == nil;
-X_pred(nils) = NaN;
+%X_pred = X;
+%[u, it] = size(X_pred);
+%nils = X_pred == nil;
+%X_pred(nils) = NaN;
 
-[X_pred, X_mean, X_std] = UserNormalization(X_pred);
+%[X_pred, X_mean, X_std] = UserNormalization(X_pred);
+X_pred = MyPCA(X, nil);
+%[mu, b_u, b_i, B] = LearnBiases(X_pred);
+%X_pred(nils) = B(nils);
 
-[mu, b_u, b_i, B] = LearnBiases(X_pred);
-X_pred(nils) = B(nils);
 
-
-[something, X_pred] = pcares(X_pred, 8);
-
-X_pred = X_pred .* X_std;
-X_pred = X_pred + X_mean;
+%[something, X_pred] = pcares(zscore(X_pred), 30);
+%X_pred = SKmeans(X_pred);
+%[something, X_pred] = pcares(X_pred, 7);
+%X_pred = X_pred .* X_std;
+%X_pred = X_pred + X_mean;
 
 
 end
